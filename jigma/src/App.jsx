d67fd5dfda4ac28c0556a0af00b051401eff8cb9
aronresni <aronresni@gmail.com>
2023-09-08 15:21:57 -0300

    resolve/deploy

--- jigma/src/App.jsx
+++ jigma/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import Secondsection from './components/section2/secondsection'
+
 import Firstsection from './components/section1/Firstsection'
 function App() {
   return (
     <div>
       <Firstsection />
-      <Secondsection />
+
     </div>
   )
 }
